b1a9fe1f21ef3f33a6d36634ac710bc19b0c5ff5
Haoran Xu <xhr0316@outlook.com>
2026-08-31 12:05:08 +0800

    feat(modules/backtester/src/__init__.py): update modules/backtester/src/__init__.py

--- modules/backtester/src/__init__.py
+++ modules/backtester/src/__init__.py
@@ -2,10 +2,13 @@
 
 from .impl.config import BacktestConfig, BacktestConfigError
 from .historical_data import HistoricalData, HistoricalDataError
+from .entry_generator import BacktestWindowError
 from .impl.engine import BacktestInputError, BacktestResult, backtest
 from .models import BacktestInput
+from .path_replay import EffectiveBacktestWindow, plan_backtest_window
 
 __all__ = (
     "BacktestConfig", "BacktestConfigError", "BacktestInput", "BacktestInputError",
-    "BacktestResult", "HistoricalData", "HistoricalDataError", "backtest",
+    "BacktestResult", "BacktestWindowError", "EffectiveBacktestWindow", "HistoricalData", "HistoricalDataError",
+    "backtest", "plan_backtest_window",
 )
