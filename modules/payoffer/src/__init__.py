f50aeaf12a32d81786b183efa3a8d59ed5fbc81d
Haoran Xu <xhr0316@outlook.com>
2026-09-05 17:15:52 +0800

    chore(modules): update __init__.py
    Update modules/payoffer/src/__init__.py

--- modules/payoffer/src/__init__.py
+++ modules/payoffer/src/__init__.py
@@ -1,6 +1,14 @@
 """Payoffer module public surface."""
 
-from .impl.engine import PayoffEngineError, preview_payload, render_payoff, run_payoff, run_runtime
+from .impl.engine import PayoffEngineError, preview_payload, render_payoff, run_resolved_payoff, run_runtime
 from .models import PayoffInput, PayoffResult
 
-__all__ = ("PayoffEngineError", "PayoffInput", "PayoffResult", "preview_payload", "render_payoff", "run_payoff", "run_runtime")
+__all__ = (
+    "PayoffEngineError",
+    "PayoffInput",
+    "PayoffResult",
+    "preview_payload",
+    "render_payoff",
+    "run_resolved_payoff",
+    "run_runtime",
+)
